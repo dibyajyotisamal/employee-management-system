95d818e133d583ea8af64c4ec4a5397e51fb4d7c
Dibyajyoti Samal <samaldibyajyoti2012@gmail.com>
2026-06-28 23:57:52 +0530

    Add the add_employee function to validate and add a new employee info to the csv file

---
+++ operations.py
@@ -0,0 +1,47 @@
+from storage import Storage
+from employee import Employee
+
+
+class EmployeeManager:
+    def __init__(self, filename):
+        self.filename = filename
+        self.storage = Storage(self.filename)
+        self.employees = self.storage.load_employees()
+
+    def list_employees(self):
+        if len(self.employees):
+            for employee in self.employees:
+                print(employee)
+        else:
+            print("No employees found.")
+
+    def add_employee(self, employee_id, name, department, email, salary):
+        # Check if the employeeID is already present
+        for employee in self.employees:
+            if employee.employee_id == employee_id:
+                print("Employee information already exists")
+                return
+        # Check if the provided email is valid
+        if "@" not in email or "." not in email:
+            print("Email is invalid")
+            return
+        # Check if the entered salary is number or not
+        try:
+            salary = float(salary)
+        except ValueError:
+            print("The salary must be a number")
+            return
+        # Check if the entered salary is positive or not
+        if float(salary) < 0:
+            print("Salary is invalid")
+            return
+
+        # After all checks are complete, create the Employee object
+        emp = Employee(employee_id, name, department, email, salary)
+
+        # Append the new Employee object to the list of Employee objects
+        self.employees.append(emp)
+
+        # Call the save_employees method to save the new employee info to the csv file
+        self.storage.save_employees(self.employees)
+        print("Employee successfully added..!!")
