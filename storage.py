b050c3bb180e4c881c75e5038a1811260d1aff40
Dibyajyoti Samal <samaldibyajyoti2012@gmail.com>
2026-06-28 20:11:05 +0530

    Implement save employees and complete the Storage class

--- storage.py
+++ storage.py
@@ -7,17 +7,31 @@ class Storage:
     def __init__(self, filename):
         self.filename = filename
 
-    def load_employees(self):
-        if os.path.exists(self.filename):
-            emp_data = csv.DictReader(self.filename)
-        else:
-            print(f"{self.filename} didn't exist. \n Creating self.filename")
-            os.mkdir(self.filename)
+    def file_is_empty(self):
+        # Check if the file exists or if file is empty before extracting information
+        return os.path.exists(self.filename) or os.path.getsize(self.filename) == 0
 
-        return []
+    def load_employees(self):
+        # Create a list to store the Employee objects
+        emp_objects = list()
+        # Open the file in read mode
+        with open(self.filename, mode="r", newline="") as file:
+            # Use the csv.DictReader() method to import all entries as a list of dictionaries
+            emp_data = csv.DictReader(file)
+            for i in emp_data:
+                # Iterate through the list and create Employee objects of each entries
+                emp = Employee(
+                    employee_id=i["employee_id"],
+                    name=i["name"],
+                    department=i["department"],
+                    email=i["email"],
+                    salary=i["salary"],
+                )
+                # Assign the employee objects to emp_objects to return
+                emp_objects.append(emp)
+
+        return emp_objects
 
     def save_employees(self, employee):
-        pass
-
-
-s = Storage("employee_data.csv")
+        with open(self.filename, mode="a", newline="") as file:
+            writer = csv.DistWriter(file, fieldname=employee.keys())
