704773605e32260d1d1d6f4c1bd21fb5749ee811
Dibyajyoti Samal <samaldibyajyoti2012@gmail.com>
2026-06-29 01:39:35 +0530

    Made changes for better email address validation

--- operations.py
+++ operations.py
@@ -1,5 +1,6 @@
 from storage import Storage
 from employee import Employee
+from email_validator import validate_email
 
 
 class EmployeeManager:
@@ -22,8 +23,10 @@ class EmployeeManager:
                 print("Employee information already exists")
                 return
         # Check if the provided email is valid
-        if "@" not in email or "." not in email:
-            print("Email is invalid")
+        try:
+            email_info = validate_email(email, check_deliverability=False)
+        except Exception as e:
+            print(f"Email is invalid. {str(e)}")
             return
         # Check if the entered salary is number or not
         try:
@@ -32,7 +35,7 @@ class EmployeeManager:
             print("The salary must be a number")
             return
         # Check if the entered salary is positive or not
-        if float(salary) < 0:
+        if salary < 0:
             print("Salary is invalid")
             return
 
@@ -45,3 +48,4 @@ class EmployeeManager:
         # Call the save_employees method to save the new employee info to the csv file
         self.storage.save_employees(self.employees)
         print("Employee successfully added..!!")
+        print(emp)
